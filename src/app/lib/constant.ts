fcbd03aa187a791c346b04735f2d52fdda878eee
BirukMengistu <sheger.shelf3@gmail>
2024-02-23 21:32:38 +0100

    add middlewaare , add detail page , add protected route, add suggest doctor

---
+++ src/app/lib/constant.ts
@@ -0,0 +1,30 @@
+type typeSocialMedia={
+ id:number,
+ url:string,
+ icon:string
+}
+
+export const SoicalMedia:typeSocialMedia[] =[
+    {
+        id:1,
+        url:'',
+        icon:'/youtube.png'
+    },
+    {
+        id:2,
+        url:'',
+        icon:'/linkedin.png'
+    }
+    ,
+    {
+        id:3,
+        url:'',
+        icon:'/twitter.png'
+    },
+    {
+        id:3,
+        url:'',
+        icon:'/facebook.png'
+    }
+
+]
